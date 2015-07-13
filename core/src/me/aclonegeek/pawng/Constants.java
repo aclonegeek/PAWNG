3c7829e998a663696893585ea382fe0e58183be6
Randy Taylor <tehgecKozzz@gmail.com>
2015-07-13 17:12:36 -0400

    Add class for constants

---
+++ core/src/me/aclonegeek/pawng/Constants.java
@@ -0,0 +1,9 @@
+package me.aclonegeek.pawng;
+
+/**
+ * Created by RandyT on 7/13/2015.
+ */
+public class Constants {
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
+}
