73204442bc58eb72a2590d2c8d0376c9d69a5379
Randy Taylor <tehgecKozzz@gmail.com>
2015-07-13 17:13:03 -0400

    Create and initialize main menu screen

---
+++ core/src/me/aclonegeek/pawng/MainMenuScreen.java
@@ -0,0 +1,72 @@
+package me.aclonegeek.pawng;
+
+import me.aclonegeek.pawng.Constants;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+/**
+ * Created by RandyT on 7/13/2015.
+ */
+public class MainMenuScreen implements Screen {
+    final PAWNG game;
+
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final PAWNG game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1280, 720);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "PAWNG", Constants.SCREEN_WIDTH / 2, Constants.SCREEN_HEIGHT / 2);
+        game.font.draw(game.batch, "Click anywhere to play", 640, 300);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(this));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
