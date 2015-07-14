fca691a50383f3e385df1f7996fbb48244debd9b
Randy Taylor <tehgecKozzz@gmail.com>
2015-07-13 23:03:19 -0400

    Update spacing? I don't know

--- core/src/me/aclonegeek/pawng/PAWNG.java
+++ core/src/me/aclonegeek/pawng/PAWNG.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class PAWNG extends Game {
 	public SpriteBatch batch;
 	public BitmapFont font;
-	
+
 	public void create() {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
