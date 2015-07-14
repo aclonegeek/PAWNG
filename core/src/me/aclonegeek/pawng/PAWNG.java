f24506de844995e62893d1e90300906566321a27
Randy Taylor <tehgecKozzz@gmail.com>
2015-07-13 22:54:59 -0400

    Remove override

--- core/src/me/aclonegeek/pawng/PAWNG.java
+++ core/src/me/aclonegeek/pawng/PAWNG.java
@@ -7,8 +7,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class PAWNG extends Game {
 	public SpriteBatch batch;
 	public BitmapFont font;
-
-	@Override
+	
 	public void create() {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
