b4f3130722a3c65b287b134679f1973d380be99e
Randy Taylor <tehgecKozzz@gmail.com>
2015-07-17 23:24:05 -0400

    Add version and modify resolution

--- desktop/src/me/aclonegeek/pawng/desktop/DesktopLauncher.java
+++ desktop/src/me/aclonegeek/pawng/desktop/DesktopLauncher.java
@@ -2,16 +2,17 @@ package me.aclonegeek.pawng.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import me.aclonegeek.pawng.Constants;
 import me.aclonegeek.pawng.PAWNG;
 
 public class DesktopLauncher {
+	private static final String VERSION = "0.0.1";
+
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 
-		cfg.title = "PAWNG";
-		cfg.width = Constants.SCREEN_WIDTH;
-		cfg.height = Constants.SCREEN_HEIGHT;
+		cfg.title = "PAWNG" + VERSION;
+		cfg.width = 1280;
+		cfg.height = 720;
 
 		new LwjglApplication(new PAWNG(), cfg);
 	}
