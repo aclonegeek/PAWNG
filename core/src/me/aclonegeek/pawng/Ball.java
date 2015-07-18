4635b26d53600674c1417cc3d4863f3919f3d3f4
Randy Taylor <tehgecKozzz@gmail.com>
2015-07-17 23:53:58 -0400

    Add Ball class

---
+++ core/src/me/aclonegeek/pawng/Ball.java
@@ -0,0 +1,43 @@
+package me.aclonegeek.pawng;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by RandyT on 7/13/2015.
+ */
+public class Ball extends GameObject {
+    private Vector2 velocity = getVelocity();
+
+    private static final float MAX_SPEED = 650f;
+    private static final float REFLECT_ANGLE = 75f;
+    private static final float SPEED = 350f;
+    private static final float SPEED_MODIFIER = 1.05f;
+
+    public Ball() {
+        super(32, 32);
+    }
+
+    public void reflect(boolean x, boolean y) {
+        if(x) velocity.x *= -1;
+        if(y) velocity.y *= -1;
+
+        setVelocity(velocity.x, velocity.y);
+    }
+
+// Getters
+    public static float getSpeed() {
+        return SPEED;
+    }
+
+    public static float getMaxSpeed() {
+        return MAX_SPEED;
+    }
+
+    public static float getReflectAngle() {
+        return REFLECT_ANGLE;
+    }
+
+    public static float getSpeedModifier() {
+        return SPEED_MODIFIER;
+    }
+}
