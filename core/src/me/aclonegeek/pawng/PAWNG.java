86f68580a0c9067cbbecfc8fad7e7c4a1a2b344a
Randy Taylor <tehgecKozzz@gmail.com>
2015-07-17 23:53:13 -0400

    Deja vu

--- core/src/me/aclonegeek/pawng/PAWNG.java
+++ core/src/me/aclonegeek/pawng/PAWNG.java
@@ -1,6 +1,7 @@
 package me.aclonegeek.pawng;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -8,11 +9,32 @@ public class PAWNG extends Game {
 	public SpriteBatch batch;
 	public BitmapFont font;
 
+	private GameState currentState = GameState.MAINMENU;
+
+	private enum GameState {
+		MAINMENU,
+		PLAY,
+		RESET,
+		EXIT,
+	}
+
 	public void create() {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
 
-		this.setScreen(new MainMenuScreen(this));
+		switch(currentState) {
+			case MAINMENU:
+				this.setScreen(new MainMenuScreen(this));
+				break;
+			case PLAY:
+				this.setScreen(new GameScreen(this));
+				break;
+			case RESET:
+				break;
+			case EXIT:
+				Gdx.app.exit();
+				break;
+		}
 	}
 
 	public void render() {
