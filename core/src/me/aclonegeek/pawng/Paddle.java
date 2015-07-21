e2120d597436c718de913459e2bd3966fb48da0d
Randy Taylor <tehgecKozzz@gmail.com>
2015-07-20 23:02:24 -0400

    Remove unneeded variable

--- core/src/me/aclonegeek/pawng/Paddle.java
+++ core/src/me/aclonegeek/pawng/Paddle.java
@@ -6,8 +6,6 @@ package me.aclonegeek.pawng;
 public class Paddle extends GameObject {
     private static final float SPEED = 400f;
 
-    public float paddleCenterY = super.getY() + (super.getHeight() / 2);
-
     public Paddle() {
         super(20, 100);
     }
