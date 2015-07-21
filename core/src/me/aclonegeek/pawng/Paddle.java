4a82e84998d7721bab2aa76488d3c785b9e797ec
Randy Taylor <tehgecKozzz@gmail.com>
2015-07-20 22:41:01 -0400

    Change member variable access modifier

--- core/src/me/aclonegeek/pawng/Paddle.java
+++ core/src/me/aclonegeek/pawng/Paddle.java
@@ -6,7 +6,7 @@ package me.aclonegeek.pawng;
 public class Paddle extends GameObject {
     private static final float SPEED = 400f;
 
-    private float paddleCenterY = super.getY() + (super.getHeight() / 2);
+    public float paddleCenterY = super.getY() + (super.getHeight() / 2);
 
     public Paddle() {
         super(20, 100);
