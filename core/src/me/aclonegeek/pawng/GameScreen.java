f382bdf44557e56553b59098ceba0be9843ac971
Randy Taylor <tehgecKozzz@gmail.com>
2015-07-23 23:54:10 -0400

    Remove unneeded import

--- core/src/me/aclonegeek/pawng/GameScreen.java
+++ core/src/me/aclonegeek/pawng/GameScreen.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
